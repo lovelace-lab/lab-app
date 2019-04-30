6a384a383f7943f8e3d2ae17727a94d03afa73fb
mottox2 <y.mottox2@gmail.com>
2019-04-30 16:17:25 +0900

    保存処理を追加

--- src/components/tips.tsx
+++ src/components/tips.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { addTips } from '../domains/tips';
+import { createTipsLocalStorage } from '../adapters/local-storage'
 
 const Tips: React.FC = () => {
   const [value, setValue] = useState('')
@@ -6,7 +8,16 @@ const Tips: React.FC = () => {
     setValue(text)
   }
 
-  return <input type="text" value={value} onChange={e => handleChange(e.target.value)}/>
+  function handleSubmit(event: React.FormEvent) {
+    const tipsLocalStorage = createTipsLocalStorage();
+    addTips(tipsLocalStorage, value)
+    event.preventDefault()
+  }
+
+  return <form onSubmit={handleSubmit}>
+    <input type="text" value={value} onChange={e => handleChange(e.target.value)} />
+    <input type="submit" value="add" />
+  </form>
 };
 
 export default Tips;
