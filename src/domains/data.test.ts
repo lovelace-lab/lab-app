e893737287c2a67c7bee4175d6ddc1579ff6b23a
SASAKI Shunsuke <erukiti@gmail.com>
2019-05-05 16:56:28 +0900

    updateDataのテストを追加

--- src/domains/data.test.ts
+++ src/domains/data.test.ts
@@ -1,4 +1,4 @@
-import { createData } from "./data";
+import { createData, updateData } from "./data";
 
 describe("createData", () => {
   test("", async () => {
@@ -11,3 +11,19 @@ describe("createData", () => {
     expect(create.mock.calls[0][1]).toEqual(metadata);
   });
 });
+
+describe("updateData", () => {
+  test("", async () => {
+    const payload = { hoge: "hoge" };
+    const metadata = { title: "title" };
+    const id = "hoge";
+    const update = jest.fn();
+
+    await updateData({ update }, id, payload, metadata);
+    expect(update.mock.calls.length).toBe(1);
+    expect(update.mock.calls[0][0]).toEqual(id);
+    expect(update.mock.calls[0][1]).toEqual(payload);
+    expect(update.mock.calls[0][2]).toEqual(metadata);
+  });
+});
+
